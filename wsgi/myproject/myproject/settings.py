b57fbaa7eb2a258af68af6b536124bd30b16036f
Dássone Yotamo <dyotamo@gmail.com>
2016-11-22 20:28:03 +0200

    Fixed JSONRenderer tuple issue ...

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -142,4 +142,4 @@ REST_FRAMEWORK = {
 }
 
 if not DEBUG:
-	REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] =  ('rest_framework.renderers.JSONRenderer',),
+	REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] =  ['rest_framework.renderers.JSONRenderer',]
