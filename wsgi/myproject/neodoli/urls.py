918eefcd37415484fd06a5872ffede1ede8cbb27
Dássone Yotamo <dyotamo@gmail.com>
2016-12-08 15:17:18 +0200

    Bower, logo and templates

--- wsgi/myproject/neodoli/urls.py
+++ wsgi/myproject/neodoli/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 
 urlpatterns = [
+	url(r'^$', TemplateView.as_view(template_name='neodoli/static/about.html')),
     url(r'^api/', include('neodoli.api.urls')),
 ]
